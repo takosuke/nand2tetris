2807bc2e1f44926127432cd32daccd9d55fd3963
Takosuke <solidparallel6890@gmail.com>
2016-06-07 00:18:24 +0200

    Finished I/O ASM assignment

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,74 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+//    @32
+//    D=A
+//    @columns
+//    M=D
+//    @16
+//    D=A
+//    @rows
+//    M=D
+    @8190
+    D=A
+    @numwords
+    M=D
+    @i
+    M=0
+//    @j
+//    M=0
+
+(LOOP)
+
+    @i
+    M=0
+    @KBD
+    D=M
+    @FILL
+    D;JGT
+    @LOOP
+    0;JMP
+
+(FILL)
+
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1
+    @i
+    M=M+1
+    @numwords
+    D=D-M
+    @FULL
+    D;JGT
+    @FILL
+    0;JMP
+
+(FULL)
+
+    @i
+    M=0
+    @KBD
+    D=M
+    @CLEAR
+    D;JEQ
+    @FULL
+    0;JMP
+
+(CLEAR)
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+    @i
+    M=M+1
+    @numwords
+    D=D-M
+    @LOOP
+    D;JGT
+    @CLEAR
+    0;JMP
