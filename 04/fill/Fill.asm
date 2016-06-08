f8d22e4b58ae9f902eda553c6089124a9ce2572f
Takosuke <solidparallel6890@gmail.com>
2016-06-08 13:51:22 +0200

    cleaned up and documented Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,74 +9,72 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
-//    @32
-//    D=A
-//    @columns
-//    M=D
-//    @16
-//    D=A
-//    @rows
-//    M=D
-    @8190
+
+
+// TODO:
+// eliminate the FULL loop?
+//  break the FILL loop if keyboard input is interrupted half way 
+
+// Setup
+
+               // Create a variable for the number of screen memory registers 
+    @8191      // 32*256 = 8192 screen memory registers to fill (-1 because zero indexed)
     D=A
     @numwords
     M=D
-    @i
-    M=0
-//    @j
-//    M=0
 
-(LOOP)
+(LOOP)         // loop while waiting for keyboard input
 
-    @i
-    M=0
-    @KBD
+    @i         // starting index for filling or emptying the screen
+    M=0        // initialize to 0
+
+    @KBD       // if the keyboard memory is not zero, jump to fill screen loop
     D=M
     @FILL
-    D;JGT
-    @LOOP
+    D;JNE
+
+    @LOOP      // else jump back to beginning of loop
     0;JMP
 
-(FILL)
+(FILL)         // screen fill loop
 
-
-    @i
+    @i         // select index (initialized to 0)
     D=M
-    @SCREEN
+    @SCREEN    // add index to screen memory base address
     A=A+D
-    M=-1
-    @i
+    M=-1       // set it to 1111111111111111111 in binary (thus, all black)
+    @i         // add 1 to index
     M=M+1
-    @numwords
+    @numwords  // check if index equals the number of screen memory locations
     D=D-M
-    @FULL
+    @FULL      // jump to full loop if it does
     D;JGT
-    @FILL
+    @FILL      // keep filling the screen otherwise
     0;JMP
 
-(FULL)
+(FULL)         // loop after screen is full
 
-    @i
+    @i         // set index back to zero
     M=0
-    @KBD
+    @KBD       // check if keyboard is not pressed (kbd memory == 0) 
     D=M
-    @CLEAR
+    @CLEAR     // jump to clear screen subroutine
     D;JEQ
-    @FULL
+    @FULL      // else stay in full loop
     0;JMP
 
-(CLEAR)
+(CLEAR)        // clear screen when key is depressed
 
-    @i
+    @i         // select index(initialized to 0 again)
     D=M
-    @SCREEN
+    @SCREEN    // select screen start address plus index
     A=A+D
-    M=0
+    M=0        // set it to 0
     @i
-    M=M+1
-    @numwords
+    M=M+1      // increment index
+    @numwords  // when index == number of screen memory locations
     D=D-M
-    @LOOP
+    @LOOP      // go back to main loop
     D;JGT
-    @CLEAR
+    @CLEAR     // else keep clearing screen
     0;JMP
